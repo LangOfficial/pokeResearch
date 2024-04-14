039943dde7f4bfe2ae257f05646c2edd49f5a92d
Langli <Langli@gmal.com>
2024-04-13 20:11:24 -0700

    revised script

--- public/build/script.js
+++ public/build/script.js
@@ -5,10 +5,7 @@ import {
   sprites,
 } from "./data/pokedexEntry.js";
 
-import {
-  pokemonNames
-} from "./data/pokemonNames.js";
-
+import { pokemonNames } from "./data/pokemonNames.js";
 
 // OVERLAY
 const overlay = document.getElementById("overlay");
@@ -20,12 +17,12 @@ const randomizeButton = document.getElementById("randomize-button");
 
 // PREVIOUS SEARCH
 const previousSearchMenu = document.getElementById("dropdown-menu-prev");
-const clearSearch = document.getElementById('clear-search');
+const clearSearch = document.getElementById("clear-search");
 
 //DROPDOWN
-const unitDropdowns = Array.from(document.querySelectorAll('.unit-dropdown'));
-const unitSelections = Array.from(document.querySelectorAll('.unit-selection'));
-const dropdownArrows = Array.from(document.querySelectorAll('.dropdown-arrow'));
+const unitDropdowns = Array.from(document.querySelectorAll(".unit-dropdown"));
+const unitSelections = Array.from(document.querySelectorAll(".unit-selection"));
+const dropdownArrows = Array.from(document.querySelectorAll(".dropdown-arrow"));
 
 // POKEMON CARD
 const pokemonCard = document.getElementById("pokemon-card");
@@ -51,22 +48,23 @@ const typeContainer = document.getElementById("type-container");
 const searchDropdown = document.getElementById("dropdown-search");
 
 // BEGINNING PRELOADER
-const beginningPreloader = document.querySelector('.beginning-preloader');
-pokeCardLoader.classList.remove('hidden');
+const beginningPreloader = document.querySelector(".beginning-preloader");
+pokeCardLoader.classList.remove("hidden");
 
 for (let i = 0; i < pokemonNames.length; i++) {
-  searchDropdown.innerHTML += `<p data-id="${i + 1}" class="text-sm cursor-pointer">#${i +1} <span class="font-bold">${pokemonNames[i]}</span></p>`
+  searchDropdown.innerHTML += `<p data-id="${i + 1}" class="text-sm cursor-pointer">#${i + 1} <span class="font-bold">${pokemonNames[i]}</span></p>`;
 }
 
 const searchOptions = Array.from(searchDropdown.children);
 for (let i = 0; i < searchDropdown.children.length; i++) {
-  searchOptions[i].addEventListener('click', () => {
-    fetchData('typedSearch', searchOptions[i].getAttribute('data-id'));
+  searchOptions[i].addEventListener("click", () => {
+    fetchData("typedSearch", searchOptions[i].getAttribute("data-id"));
   });
-};
+}
 
 //might need to go based on the pokemon-id or a data-value
-let ready = true;
+let ready = JSON.parse(localStorage.getItem("ready")) && true;
+
 let generatedContainers = Array.from(
   document.querySelectorAll(".generated-container")
 );
@@ -74,28 +72,27 @@ let generatedContainers = Array.from(
 // DATA LOAD
 let prevSearches = JSON.parse(localStorage.getItem("prevSearches")) || {};
 
-let prevSearchIds = JSON.parse(localStorage.getItem('prevIds')) || [];
+let prevSearchIds = JSON.parse(localStorage.getItem("prevIds")) || [];
 
-let currCardId = JSON.parse(localStorage.getItem('currCardId')) || 1;
+let currCardId = JSON.parse(localStorage.getItem("currCardId")) || 1;
 
 for (let i = 0; i < unitDropdowns.length; i++) {
-  unitDropdowns[i].addEventListener('click', () => {
-    unitSelections[i].classList.toggle('hidden');
-    dropdownArrows[i].classList.toggle('fa-angle-up');
-    Array.from(unitSelections[i].children).forEach(unit => {
-      unit.addEventListener('click', () => {
+  unitDropdowns[i].addEventListener("click", () => {
+    unitSelections[i].classList.toggle("hidden");
+    dropdownArrows[i].classList.toggle("fa-angle-up");
+    Array.from(unitSelections[i].children).forEach((unit) => {
+      unit.addEventListener("click", () => {
         //maybe an animation instead then hide
-        unitSelections[i].classList.add('hidden');
-        dropdownArrows[i].classList.toggle('fa-angle-up');
-      })
-    })
-  })
+        unitSelections[i].classList.add("hidden");
+        dropdownArrows[i].classList.toggle("fa-angle-up");
+      });
+    });
+  });
 }
 
 if (prevSearchIds.length === 0) {
   addNothingMsg();
-}
-else {
+} else {
   for (const prevId of prevSearchIds) {
     previousSearchMenu.innerHTML += prevSearches[prevId];
   }
@@ -103,20 +100,22 @@ else {
 
 prevItemFunc();
 
-async function fetchData(mode, pokemon="") {
+async function fetchData(mode, pokemon = "") {
   try {
     randomizeButton.disabled = true;
     let pokemonName = "";
-    if (ready && mode !== 'currLoad') {
+    if (ready) {
       pokemonName = "bulbasaur";
     } else {
       if (mode === "random") {
         pokemonName = Math.floor(Math.random() * 1008) + 1;
-      } 
-      else if (mode === 'prevSearch' || mode === 'currLoad' || mode === 'typedSearch') {
+      } else if (
+        mode === "prevSearch" ||
+        mode === "currLoad" ||
+        mode === "typedSearch"
+      ) {
         pokemonName = pokemon;
-      }
-      else {
+      } else {
         pokemonName = searchBar.value.toLowerCase().replaceAll(" ", "-");
       }
     }
@@ -129,40 +128,45 @@ async function fetchData(mode, pokemon="") {
     }
 
     const data = await response.json();
-     
+
     // UPDATE STYLES
     const response2 = await fetch(
       `https://pokeapi.co/api/v2/pokemon-species/${data.id}/`
     );
 
     const speciesData = await response2.json();
-    // if (mode === 'currLoad') {
-    //   const prevElem = document.getElementById(data.id);
-    //   if (prevElem) {
-    //     prevElem.remove();
-    //   }
-    // }
-    
-    if ((!prevSearchIds.includes(data.id) || mode === "currLoad") || mode === "prevSearch" || mode === 'typedSearch') {
+    if (mode === "currLoad") {
+      const prevElem = document.getElementById(data.id);
+      if (prevElem) {
+        prevElem.remove();
+      }
+    }
+
+    if (
+      !prevSearchIds.includes(data.id) ||
+      mode === "currLoad" ||
+      mode === "prevSearch" ||
+      mode === "typedSearch"
+    ) {
       pokeCardLoader.style.display = "flex";
-      pokemonCard.style.display = "none";  
+      pokemonCard.style.display = "none";
       localStorage.setItem("currCardId", JSON.stringify(data.id));
       const pokemonColor = speciesData.color.name;
 
       generatedContainers.forEach((container) => {
         container.innerHTML = "";
       });
-  
+
       pokemonCard.classList.forEach((className) => {
         if (className.startsWith("border-") && className.endsWith("0")) {
           pokemonCard.classList.remove(className);
         }
       });
-  
+
       pokemonCard.classList.add(
         `border-${pokemonColor}-${pokemonColors[pokemonColor]}`
       );
-     
+
       // LOAD BASIC INFO
       const displayName = title(data.name, "-", " ");
       introInfo.innerHTML += `<h1 id="pokemon-name" class="font-bold text-3xl">${displayName}</h1>`;
@@ -174,7 +178,7 @@ async function fetchData(mode, pokemon="") {
       }
       // you actually could just include it in the innerhtml in the typecontainer. but ill keep this to remind myself that I did it wrong
       updateTypeDisplays();
-  
+
       // LOAD SPRITES
       for (const sprite of sprites) {
         const spriteUrl = data.sprites[sprite];
@@ -186,11 +190,11 @@ async function fetchData(mode, pokemon="") {
           </div>`;
         }
       }
-  
+
       // LOAD AND CALCULATE WEIGHT AND HEIGHT
       let weight = data.weight / 10;
       let height = data.height / 10;
-  
+
       weight = formatPokemonMeas(weight);
       height = formatPokemonMeas(height);
       const heightFt = height * 3.28084;
@@ -198,21 +202,21 @@ async function fetchData(mode, pokemon="") {
       const pokemonWeight = `${weight}kg (${(weight * 2.20462).toFixed(1)}lbs)`;
       const pokemonHeight = `${height}m (${heightFt.toFixed(0)}"${Math.round(heightIn)}')`;
       const measurements = { Height: pokemonHeight, Weight: pokemonWeight };
-  
+
       for (const [text, measurement] of Object.entries(measurements)) {
         measurementContainer.innerHTML += `<p><span class="font-semibold">${text}:</span> ${measurement}</p>`;
       }
-  
+
       // LOAD ABILITIES
       let prevAbilities = []; // for a few exceptions
       for (const ab of data.abilities) {
         const abilityTitle = title(ab.ability.name, "-", " ");
         const abResponse = await fetch(ab.ability.url);
-  
+
         if (!abResponse.ok) {
           throw new Error("Unable to fetch resource :(");
         }
-  
+
         const abData = await abResponse.json();
         let abilityDesc = "Work in progress...";
         for (const ab2 of abData.effect_entries) {
@@ -221,7 +225,7 @@ async function fetchData(mode, pokemon="") {
             break;
           }
         }
-  
+
         if (!ab.is_hidden) {
           const abilityText = `${ab.slot}. ${abilityTitle}`;
           pokemonAbilityContainer.innerHTML += `<div href="" class="ability-display">
@@ -239,7 +243,7 @@ async function fetchData(mode, pokemon="") {
         }
         prevAbilities.push(abilityTitle);
       }
-      
+
       // LOAD STATS
       let totalStat = 0;
       for (let i = 0; i < data.stats.length; i++) {
@@ -249,9 +253,9 @@ async function fetchData(mode, pokemon="") {
        <p>`;
         totalStat += stat.base_stat;
       }
-  
+
       pokemonStatsContainer.innerHTML += `<p><span class="font-semibold">Total</span>: ${totalStat}</p>`;
-  
+
       // LOAD CRIES
       for (const [cryName, cryUrl] of Object.entries(data.cries)) {
         if (cryUrl) {
@@ -266,13 +270,13 @@ async function fetchData(mode, pokemon="") {
       }
       if (!ready) {
         const prevEntry = document.getElementById(data.id);
-        if (mode === 'prevSearch' || (mode === 'typedSearch' && prevEntry) || mode === 'currLoad') {
-          if (prevEntry) {
-            prevEntry.remove();
-          }
-        }
-        // DROP ITEM
-        const dropItemStr = `
+        // if (mode === 'prevSearch' || (mode === 'typedSearch' && prevEntry) || mode === 'currLoad') {
+        //   if (prevEntry) {
+        //     prevEntry.remove();
+        //   }
+        //         }
+        if (!prevEntry) {
+          const dropItemStr = `
           <a class="dropdown-item" id="${data.id}">
             <img class="w-14" src="${data.sprites.front_default}" />
             <div>
@@ -280,29 +284,36 @@ async function fetchData(mode, pokemon="") {
               <p>${displayName}</p>
             </div>
           </a>`;
-        document.getElementById('clear-search').insertAdjacentHTML('afterend', dropItemStr);
-        const currSearch = {};
-        currSearch[data.id] = dropItemStr;
+          document
+            .getElementById("clear-search")
+            .insertAdjacentHTML("afterend", dropItemStr);
+          const currSearch = {};
+          currSearch[data.id] = dropItemStr;
 
-        prevSearches = {...currSearch, ...prevSearches};
-        prevSearchIds.unshift(data.id);
-        
-        const currSearchElem = document.getElementById(data.id);
-        currSearchElem.addEventListener("click", () => {
-         fetchData('prevSearch', data.id)});
+          prevSearches = { ...currSearch, ...prevSearches };
+          if (!prevSearchIds.includes(data.id)) {
+            prevSearchIds.unshift(data.id);
+          }
 
-        localStorage.setItem("prevSearches", JSON.stringify(prevSearches));
-        localStorage.setItem("prevIds", JSON.stringify(prevSearchIds));
-  
-        const nothingMsg = document.body.querySelector('.nothing-msg');
-        if (nothingMsg) {
-          nothingMsg.remove();
+          const currSearchElem = document.getElementById(data.id);
+          currSearchElem.addEventListener("click", () => {
+            fetchData("prevSearch", data.id);
+          });
+
+          localStorage.setItem("prevSearches", JSON.stringify(prevSearches));
+          localStorage.setItem("prevIds", JSON.stringify(prevSearchIds));
+
+          const nothingMsg = document.body.querySelector(".nothing-msg");
+          if (nothingMsg) {
+            nothingMsg.remove();
+          }
         }
+        // DROP ITEM
       }
-      
+
       ready = false;
+      localStorage.setItem("ready", JSON.stringify(ready));
     }
-    
   } catch (error) {
     if (error.message === "Not Found") {
       alert("Pokemon not found!");
@@ -343,18 +354,18 @@ function title(s, startSeparator, endSeparator) {
 }
 
 function prevItemFunc() {
-  previousSearchMenu.querySelectorAll('.dropdown-item').forEach(item => {
-    item.addEventListener('click', e => {
-      fetchData('prevSearch', item.id);
+  previousSearchMenu.querySelectorAll(".dropdown-item").forEach((item) => {
+    item.addEventListener("click", (e) => {
+      fetchData("prevSearch", item.id);
     });
-  })
+  });
 }
 
 searchEnter.addEventListener("click", fetchData);
 
 searchBar.addEventListener("keypress", (e) => {
   if (e.key === "Enter") {
-    fetchData('typedSearch', searchBar.value);
+    fetchData("typedSearch", searchBar.value);
   }
 });
 
@@ -363,13 +374,14 @@ randomizeButton.addEventListener("click", () => {
 });
 
 function addNothingMsg() {
-  previousSearchMenu.innerHTML += '<p class="pointer-events-none nothing-msg text-center p-3">Nothing...</p>';
+  previousSearchMenu.innerHTML +=
+    '<p class="pointer-events-none nothing-msg text-center p-3">Nothing...</p>';
 }
 
 function addClearEvent() {
-  document.getElementById('clear-search').addEventListener('click', () => {
+  document.getElementById("clear-search").addEventListener("click", () => {
     if (prevSearchIds.length > 0) {
-      previousSearchMenu.querySelectorAll('.dropdown-item').forEach(item => {
+      previousSearchMenu.querySelectorAll(".dropdown-item").forEach((item) => {
         item.remove();
       });
       addNothingMsg();
@@ -378,37 +390,37 @@ function addClearEvent() {
       localStorage.setItem("prevSearches", JSON.stringify(prevSearches));
       localStorage.setItem("prevIds", JSON.stringify(prevSearchIds));
     }
-  });  
+  });
 }
 
-searchBar.addEventListener("input", e => {
+searchBar.addEventListener("input", (e) => {
   const value = searchBar.value.toLowerCase();
   for (let i = 0; i < pokemonNames.length; i++) {
     const name = pokemonNames[i].toLowerCase();
     const isVisible = name.includes(value);
     const pokemonEntry = document.querySelector(`[data-id="${i + 1}"`);
-    pokemonEntry.classList.toggle('hidden', !isVisible);
+    pokemonEntry.classList.toggle("hidden", !isVisible);
   }
 });
 
-document.body.addEventListener('click', e => {
+document.body.addEventListener("click", (e) => {
   if (e.target !== searchBar) {
-    searchDropdown.classList.add('hidden');
+    searchDropdown.classList.add("hidden");
   }
 });
 
 addClearEvent();
 
-searchBar.addEventListener('click', () => {
-  searchDropdown.classList.toggle('hidden');
-})
+searchBar.addEventListener("click", () => {
+  searchDropdown.classList.toggle("hidden");
+});
 
-window.addEventListener('load', () => {
-  fetchData('currLoad', currCardId);
-  beginningPreloader.style.display = 'none';
-  pokemonCard.style.display = 'flex';
-})
+window.addEventListener("load", () => {
+  fetchData("currLoad", currCardId);
+  beginningPreloader.style.display = "none";
+  pokemonCard.style.display = "flex";
+});
 
-previousSearchMenu.addEventListener('animationstart', () => {
+previousSearchMenu.addEventListener("animationstart", () => {
   addClearEvent();
-})
+});
